4c2bc2c71a090cc1c3ce73dd1d2e46790c612e28
WalterG16 <waltergriva16@gmail.com>
2024-06-04 00:58:25 -0300

    refactory code

--- models/index.js
+++ models/index.js
@@ -2,7 +2,7 @@ import Propiedad from './Propiedad.js';
 import Precio from './Precio.js';
 import Categoria from './Categoria.js';
 import Usuario from './Usuario.js';
-import Mensaje from './mensaje.js';
+import Mensaje from './Mensaje.js';
 
 
 //Precio.hasOne(Propiedad);
